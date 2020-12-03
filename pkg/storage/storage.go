a63240d62191a0eb3fd1a699db0694c61a12289b
Mahi Fernando <mahifernando@gmail.com>
2020-12-03 01:42:42 +0000

    Implement file methods.

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,7 +1,12 @@
 package storage
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
+
+	"gocv.io/x/gocv"
 )
 
 // Implement DirectoryProvider interface.
@@ -33,4 +38,29 @@ func CreateIfDirNotExists(provider DirectoryProvider, path string) (bool, error)
 	}
 
 	return true, nil
+}
+
+// Implement FileProvider interface
+// Save image file represent by opencv multi-dimensional array
+func (f *File) Create(path string) bool {
+	return gocv.IMWrite(path, f.Data)
+}
+
+// Generate unique image file name for saving to disk
+// Unique name is time sequenced for sorting.
+// I.E. Example file name looks like ./directory-name/base-file-name.<unix-nanoseconds>.jpeg
+func GenerateSequenceImageName(seq SequenceProvider, dir string, baseName string, imageType string) string {
+	return filepath.Join(dir, fmt.Sprintf("%s-%s.%s", baseName, seq.Next(), imageType))
+}
+
+// Implement SequenceProvider interface
+// Generate time based next id (Unix time nano seconds)
+func (s *SequenceId) Next() string {
+	return fmt.Sprint(time.Now().UnixNano())
+}
+
+// Save opencv multi-dimensional arrary data image (frame) to disk as 'JPEG' file.
+func SaveImageToDisk(provider FileProvider, seq SequenceProvider, dir string, baseImageName string) bool {
+	image := GenerateSequenceImageName(seq, dir, baseImageName, "jpeg")
+	return provider.Create(image)
 }
