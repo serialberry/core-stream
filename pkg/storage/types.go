a78dc9a3e3043c53c313faa7be346bd4b70060bc
Mahi Fernando <mahifernando@gmail.com>
2020-12-03 01:42:15 +0000

    Add storage file type, sequence id

--- pkg/storage/types.go
+++ pkg/storage/types.go
@@ -1,5 +1,7 @@
 package storage
 
+import "gocv.io/x/gocv"
+
 type DirectoryProvider interface {
 	IsExists(path string) bool // determine if named directory exists in the file system.
 	Create(path string) error  // create named directory along with parent dir(s).
@@ -8,3 +10,18 @@ type DirectoryProvider interface {
 type Directory struct {
 	Path string
 }
+
+type FileProvider interface {
+	Create(path string) bool
+}
+
+type File struct {
+	Data gocv.Mat
+	Path string
+}
+
+type SequenceProvider interface {
+	Next() string
+}
+
+type SequenceId string
